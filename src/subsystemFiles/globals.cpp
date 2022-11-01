96f72ae3e19f634652012034f557173454f3f59b
Albert Tran <thefrogOnething@gmail.com>
2022-10-31 20:01:09 -0700

    uh fixed reverse issues might spin correctly now

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
 
-pros::Motor leftDrive1(1, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor leftDrive2(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor leftDrive3(4, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftDrive1(1, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftDrive2(2, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftDrive3(4, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
 
 pros::Motor rightDrive1(11, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor rightDrive2(12, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
