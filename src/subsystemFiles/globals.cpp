0a52251fbef61fc523cefaf8c645b803b02a3c01
Albert Tran <thefrogOnething@gmail.com>
2022-10-31 17:44:30 -0700

    fixed ports

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -2,7 +2,7 @@
 
 pros::Motor leftDrive1(1, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor leftDrive2(2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor leftDrive3(3, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftDrive3(4, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 pros::Motor rightDrive1(11, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor rightDrive2(12, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
