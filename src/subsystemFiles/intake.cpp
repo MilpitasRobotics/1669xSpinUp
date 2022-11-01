280fb899533cb929325a58d19b8ae0e719e456e0
Albert Tran <thefrogOnething@gmail.com>
2022-10-31 23:36:49 -0700

    intake code should be working using L1 & R1

--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -2,13 +2,17 @@
 #include "okapi/api.hpp"
 
 
-void setIntake(int velocity) {
-    motorintake = velocity;
+void setIntake(int power) {
+    motorintake = power;
 }
 
 
-//think about math and booleans, if l1 isn't pressed, it's false, giving it a value of 0, if it is pressed, it's true, giving it a value of 1
-
 void setIntakeMotor() {
-    int intakePower = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+    int intakePower = 0;
+    if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+        intakePower = -127;
+    else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        intakePower = 127;
+    else{}
+        intakePower = 0;
 }
