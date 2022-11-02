505dcf828b5e8ace0a65035f041f1887d5d6dc7b
Sawan Srivastava <49292654+SawanSrivastava1210@users.noreply.github.com>
2022-11-02 12:57:45 -0700

    fixed undefined reference error

--- src/subsystemFiles/intake.cpp
+++ src/subsystemFiles/intake.cpp
@@ -7,7 +7,7 @@ void setIntake(int power) {
 }
 
 
-void setIntakeMotor() {
+void setIntakeMotors() {
     int intakePower = 0;
     if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
         intakePower = -127;
