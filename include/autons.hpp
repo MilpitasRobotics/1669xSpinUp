b5f3661e7089f6c7e0cdb126de4142d0ad5fc7cb
Sawan Srivastava <49292654+SawanSrivastava1210@users.noreply.github.com>
2022-11-28 16:33:08 -0800

    First commit for EZTemplate

---
+++ include/autons.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "EZ-Template/drive/drive.hpp"
+
+extern Drive chassis;
+
+void drive_example();
+void turn_example();
+void drive_and_turn();
+void wait_until_change_speed();
+void swing_example();
+void combining_movements();
+void interfered_example();
+void leftSide();
+void test_constants(double inches);
+double convert_encoder_values(int port);
+
+void default_constants();
+void one_mogo_constants();
+void two_mogo_constants();
+void exit_condition_defaults();
+void modified_exit_condition();
