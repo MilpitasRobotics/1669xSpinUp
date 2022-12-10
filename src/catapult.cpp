fd089463bef6d28e1d24e8bb9acf29c42ab45f68
Albert Tran <thefrogOnething@gmail.com>
2022-12-10 15:42:51 -0800

    change motor behavior while switch is engaged

--- src/catapult.cpp
+++ src/catapult.cpp
@@ -11,7 +11,7 @@ void move_catapult(){
       }
     if (catapult_switch.get_value()){
       if(master.get_digital(DIGITAL_R1)){ // catapult code should work smt like this, need to confirm w/ Eugene how exactly it works
-       catapultMotor.move_relative(30, 20); // need to figure out the right units so that the slip gear slips
+       catapultMotor.move_velocity(20); // test to see if 20 velocity is right 
     }
     }
 }
