2886d64b2dacc7c0163e5139fc2766ce0c232e97
Sawan Srivastava <49292654+SawanSrivastava1210@users.noreply.github.com>
2023-02-16 11:56:29 -0800

    added Prog skills call in driver control

--- src/main.cpp
+++ src/main.cpp
@@ -184,22 +184,5 @@ pros::Task loadInDriver{[=] { // lambda (anonymous)function for load
   }
   }};
 
-  chassis.set_drive_brake(MOTOR_BRAKE_COAST);  
-
-  while (true) {
-    move_catapult(600);    
-    // chassis.tank(); // Tank control
-    chassis.arcade_standard(ez::SPLIT); // Standard split arcade
-    // chassis.arcade_standard(ez::SINGLE); // Standard single arcade
-    // chassis.arcade_flipped(ez::SPLIT); // Flipped split arcade
-    // chassis.arcade_flipped(ez::SINGLE); // Flipped single arcade
-    
-    // . . .
-    // Put more user control code here!
-    // . . .
-    activateEndgame();
-    move_intake_roller();
-    
-    pros::delay(ez::util::DELAY_TIME); // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
-  }
+  progSkills(); 
 }
